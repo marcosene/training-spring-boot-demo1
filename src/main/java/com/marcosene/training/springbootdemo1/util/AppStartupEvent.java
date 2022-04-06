19517ef34fdf7773a7742d4f7757ad8891d83dff
Marco Sene <marcosene@hotmail.com>
2022-04-06 12:29:30 +0200

    Spring data first settings

---
+++ src/main/java/com/marcosene/training/springbootdemo1/util/AppStartupEvent.java
@@ -0,0 +1,23 @@
+package com.marcosene.training.springbootdemo1.util;
+
+import com.marcosene.training.springbootdemo1.data.Room;
+import com.marcosene.training.springbootdemo1.data.RoomRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final RoomRepository roomRepository;
+
+    public AppStartupEvent(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        rooms.forEach(System.out::println);
+    }
+}
